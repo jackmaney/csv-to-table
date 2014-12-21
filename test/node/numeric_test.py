37db4fe5dae01ae966169b279016043b96fd0491
Jack Maney <jackmaney@gmail.com>
2014-12-21 16:18:41 -0600

    Starting on tests...

---
+++ test/node/numeric_test.py
@@ -0,0 +1,40 @@
+from node.numeric import NumericRangeNode, IntRangeNode
+from nose.tools import ok_
+
+
+def test_int_range_node():
+
+    int_range_node = IntRangeNode('int_range', lower=0, upper=10)
+
+    too_low = -1
+    too_high = 15
+    just_right = 5
+    wtf = 'hurr durr!'
+
+    ok_(not int_range_node.indicator_function(too_low),
+        "Kept out numbers that are too low")
+    ok_(not int_range_node.indicator_function(too_high),
+        "Kept out numbers that are too high")
+    ok_(int_range_node.indicator_function(just_right),
+        "Keep in numbers between the range")
+    ok_(not int_range_node.indicator_function(wtf),
+        "Reject things that aren't numbers")
+
+
+def test_numeric_range_node():
+
+    num_range_node = NumericRangeNode('nrange', lower=-3.1, upper=2.71828)
+
+    too_low = -4.5
+    too_high = 3
+    just_right = 1 + 1e-5
+    wtf = "blarg"
+
+    ok_(not num_range_node.indicator_function(too_low),
+        "Kept out numbers that are too low")
+    ok_(not num_range_node.indicator_function(too_high),
+        "Kept out numbers that are too high")
+    ok_(num_range_node.indicator_function(just_right),
+        "Keep in numbers between the range")
+    ok_(not num_range_node.indicator_function(wtf),
+        "Reject things that aren't numbers")
